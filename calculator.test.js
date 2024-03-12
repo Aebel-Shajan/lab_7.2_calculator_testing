0b39322d6fde25ff089d9c0ef010678a112d5727
Aebel Shajan <aebelshajan@gmail.com>
2024-03-12 12:45:37 +0000

    initial commit. Copy start code

---
+++ calculator.test.js
@@ -0,0 +1,55 @@
+const {
+  sum,
+  subtract,
+  multiply,
+  divide,
+  modulus,
+  even,
+  odd,
+} = require("./calculator");
+
+describe('sum', () => { 
+  
+  test('can add two small positive numbers', () => {
+    expected = 5;
+    actual = sum(2, 3);
+    expect(actual).toBe(expected);
+  });
+
+  test.skip('can add two large positive numbers', () => { 
+    
+  });
+
+  test.skip('can add two negative numbers', () => {
+    
+  });
+
+  test.skip('can add zero', () => {
+    
+  });
+
+});
+
+describe('subtract', () => {
+
+});
+
+describe('multiply', () => {
+
+});
+
+describe('divide', () => {
+
+});
+
+describe('modulus', () => {
+
+});
+
+describe('even', () => {
+
+});
+
+describe('odd', () => {
+
+});
